4b315510cc5d5d13dbb5b85eee1cb11415ee29c2
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-19 17:53:57 -0500

    comment section placed as step in fish modal

--- lib/src/utils/widgets/card_carousel_image.dart
+++ lib/src/utils/widgets/card_carousel_image.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:login_flutter/src/models/comment.dart';
+import 'package:login_flutter/src/utils/widgets/text_view.dart';
 
 import '../../models/peces_model.dart';
 import 'base_modal.dart';
+
+enum StepView {
+  initial,
+  interactionView,
+}
 
 class CardImages extends StatefulWidget {
   final Pez pez;
@@ -24,12 +30,8 @@ class CardImagesState extends State<CardImages> {
             onTap: () => showDialog(
               context: context,
               builder: (context) {
-                return BaseModal(
-                  heightFactor: .65,
-                  widthFactor: .9,
-                  content: SingleChildScrollView(
-                    child: _buildFishModalContent(),
-                  ),
+                return InfoModal(
+                  pez: widget.pez,
                 );
               },
             ),
@@ -41,80 +43,6 @@ class CardImagesState extends State<CardImages> {
           ),
         ),
         _buildLikeBttn(),
-      ],
-    );
-  }
-
-  Widget _buildFishModalContent() {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: FadeInImage(
-                  placeholder: const AssetImage("assets/loading3.gif"),
-                  image: AssetImage(widget.pez.imageRoute),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.pez.name,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    widget.pez.description,
-                    textAlign: TextAlign.justify,
-                    style: const TextStyle(
-                      color: Colors.yellow,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  SingleChildScrollView(
-                    child: Container(
-                        color: Colors.grey,
-                        height: 200,
-                        child: ListView.builder(
-                          itemCount: widget.pez.comments?.length ?? 0,
-                          itemBuilder: (context, index) {
-                            Comment comment = widget.pez.comments![index];
-                            return ListTile(
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(comment.username),
-                                  Text(comment.mail)
-                                ],
-                              ),
-                              subtitle: Text(comment.comment),
-                            );
-                          },
-                        )),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
       ],
     );
   }
@@ -133,6 +61,122 @@ class CardImagesState extends State<CardImages> {
           color: widget.pez.isLiked ? Colors.red : Colors.grey,
         ),
       ),
+    );
+  }
+}
+
+class InfoModal extends StatefulWidget {
+  final Pez pez;
+
+  const InfoModal({super.key, required this.pez});
+
+  @override
+  State<InfoModal> createState() => _InfoModalState();
+}
+
+class _InfoModalState extends State<InfoModal> {
+  StepView step = StepView.initial;
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseModal(
+      heightFactor: .65,
+      widthFactor: .9,
+      paddingValue: 10,
+      content: _buildFishModalContent(),
+    );
+  }
+
+  Widget _buildFishModalContent() {
+    if (StepView.initial == step) {
+      return Stack(
+        children: [
+          Column(
+            children: [
+              _buildHeaderInformation(),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() => step = StepView.interactionView);
+                },
+                child: const TextView(text: 'see coments'),
+              )
+            ],
+          ),
+        ],
+      );
+    } else if (StepView.interactionView == step) {
+      return SingleChildScrollView(
+        child: _buildInteractionSection(),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _buildHeaderInformation() {
+    return Column(
+      children: [
+        Text(
+          widget.pez.name,
+          style: const TextStyle(
+            color: Colors.grey,
+            fontSize: 20,
+          ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: FadeInImage(
+            placeholder: const AssetImage("assets/loading3.gif"),
+            image: AssetImage(widget.pez.imageRoute),
+            fit: BoxFit.cover,
+          ),
+        ),
+        Text(
+          widget.pez.description,
+          textAlign: TextAlign.justify,
+          style: const TextStyle(
+            color: Colors.yellow,
+            fontSize: 14,
+          ),
+        ),
+        const Divider(
+          color: Colors.black,
+          thickness: 1,
+        )
+      ],
+    );
+  }
+
+  Widget _buildInteractionSection() {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() => step = StepView.initial);
+          },
+          child: const TextView(text: 'back'),
+        ),
+        Container(
+          color: Colors.grey,
+          height: 200,
+          child: ListView.builder(
+            itemCount: widget.pez.comments?.length ?? 0,
+            itemBuilder: (context, index) {
+              Comment comment = widget.pez.comments![index];
+              return ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text(comment.username), Text(comment.mail)],
+                ),
+                subtitle: Text(comment.comment),
+              );
+            },
+          ),
+        ),
+        TextFormField(
+          maxLines: 5,
+        )
+      ],
     );
   }
 }

3f5909042d54af5561d1664ce89b2ec7c9438b8d
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-21 23:26:45 -0500

    bttn added to modal about us

--- lib/src/subViews/about_us_view.dart
+++ lib/src/subViews/about_us_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_flutter/src/utils/widgets/custom_button.dart';
 import 'package:login_flutter/src/utils/widgets/text_view.dart';
 
 class AboutUsView extends StatelessWidget {
@@ -8,34 +9,41 @@ class AboutUsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         children: [
-          TextView(
+          const TextView(
             text: 'Acerca de la Aplicación para Acuarios',
             textAlign: TextAlign.center,
             fontWeight: FontWeight.bold,
           ),
-          SizedBox(height: 20),
-          TextView(
+          const SizedBox(height: 20),
+          const TextView(
             text: 'Bienvenido a la Aplicación para Acuarios',
             fontWeight: FontWeight.w900,
             fontSize: 20,
             textAlign: TextAlign.center,
           ),
-          SizedBox(height: 20),
-          TextView(
+          const SizedBox(height: 20),
+          const TextView(
             text:
                 'Esta aplicación está diseñada para los amantes de los acuarios, ya sean principiantes o expertos en acuarismo. Aquí encontrarás herramientas útiles, información sobre peces, consejos de cuidado y mucho más.',
             textAlign: TextAlign.justify,
           ),
-          SizedBox(height: 10),
-          TextView(
+          const SizedBox(height: 10),
+          const TextView(
             text:
                 '¡Explora el mundo fascinante de los acuarios y crea un entorno acuático vibrante para tus peces!',
             textAlign: TextAlign.justify,
             fontWeight: FontWeight.w600,
           ),
+          const SizedBox(height: 35),
+          CustomButton(
+            buttonTextColor: Colors.black,
+            isAppColor: false,
+            text: 'Entendido!',
+            onPressed: () => Navigator.of(context).pop(),
+          )
         ],
       ),
     );

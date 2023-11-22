e91f4bc27225a2ca083e46b00fed3bde57e5e98c
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-22 13:37:05 -0500

    close button creted

---
+++ lib/src/utils/widgets/close_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CloseBttn extends StatelessWidget {
+  const CloseBttn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Container(
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                spreadRadius: 4,
+                color: Color.fromARGB(45, 0, 0, 0),
+                blurRadius: 7),
+          ],
+        ),
+        child: const CircleAvatar(
+          radius: 12,
+          backgroundColor: Colors.black,
+          child: Icon(
+            Icons.close,
+            color: Colors.white,
+            size: 15,
+          ),
+        ),
+      ),
+      onPressed: () => Navigator.of(context).pop(),
+    );
+  }
+}

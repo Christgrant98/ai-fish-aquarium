bec8069670aa1829004525f6f922ff2073e7e041
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-23 16:37:33 -0500

    trivia model updated

--- lib/src/models/trivia.dart
+++ lib/src/models/trivia.dart
@@ -3,11 +3,13 @@ class TriviaGame {
   List<String> answers;
   String imagePath;
   int points;
+  bool isCorrect;
 
   TriviaGame({
     required this.question,
     required this.answers,
     required this.imagePath,
     required this.points,
+    required this.isCorrect,
   });
 }

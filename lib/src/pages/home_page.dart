a14412c58e745977485b6103092f658549eff972
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-24 18:42:00 -0500

    home page refact

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:login_flutter/src/utils/widgets/layout.dart';
 
 import '../utils/widgets/custom_button.dart';
-import '../utils/widgets/text_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,24 +12,82 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Layout(
       hasBottomNavigationBar: false,
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              onPressed: () {
-                context.go("/login");
-              },
-              text: "Iniciar Sesión",
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * .9,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: _buildHeaderTitle(),
+                ),
+                const SizedBox(height: 30),
+                CircleAvatar(
+                    backgroundColor: Colors.teal,
+                    radius: 175,
+                    child: Image.asset(
+                      'assets/fish_home.png',
+                      height: 250,
+                      width: 250,
+                    )),
+                const SizedBox(height: 30),
+                CustomButton(
+                  onPressed: () {
+                    context.go("/login");
+                  },
+                  text: "Iniciar Sesión",
+                ),
+                const SizedBox(height: 10),
+                CustomButton(
+                  onPressed: () {
+                    context.go("/registro");
+                  },
+                  color: Colors.teal,
+                  text: "Regístrate",
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+
+  RichText _buildHeaderTitle() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        style: GoogleFonts.quicksand(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 27.5,
+        ),
+        children: const [
+          TextSpan(
+            text: 'Welcome to the ',
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 40,
+              color: Colors.black,
             ),
-            CustomButton(
-              onPressed: () {
-                context.go("/registro");
-              },
-              color: Colors.teal,
-              text: "Regístrate",
-            )
-          ]),
+          ),
+          TextSpan(
+            text: 'Aquarium',
+            style: TextStyle(
+              color: Colors.teal, // Color azul
+              fontWeight: FontWeight.w900,
+              fontSize: 40,
+            ),
+          ),
+          TextSpan(
+            text: ' app',
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 40,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
